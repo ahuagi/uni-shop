c76253733c83b26b0b29d07d5067165c51e55004
ahua <wyh2524136172@icloud.com>
2023-01-03 16:06:28 +0800

    主页面编写

--- main.js
+++ main.js
@@ -3,6 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 
+// 网络请求的配置
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+
+// 定义展示数据请求状态的方法
+uni.$showMsg = function(title = "数据请求失败" , duration = 1500){
+	uni.showToast({
+		title , 
+		duration ,
+		icon:'none'
+	})
+}
+
+// 基础请求地址
+$http.baseUrl = 'https://www.uinav.com'
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title : '数据加载中'
+	})
+}
+
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
